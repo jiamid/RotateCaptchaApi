80d91fa6d9889a463fd7a38219f4e86741091b6e
jiamid <jiamid@qq.com>
2022-03-14 19:57:37 +0800

    init

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM python:3.7
+ENV PYTHONUNBUFFERED 1
+# RUN apt-get update
+# RUN curl -fsSL https://deb.nodesource.com/setup_16.x | bash -
+# RUN apt-get install -y nodejs
+RUN mkdir /code
+WORKDIR /code
+COPY requirements.txt /code/
+RUN pip install -i https://pypi.douban.com/simple -U pip
+RUN pip install -i https://pypi.douban.com/simple -r requirements.txt
+COPY . /code/
---
+++ src/main_api.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+from uvicorn import run
+
+from api.rotate_angle import router
+
+
+API_HEADERS = '/rotate_captcha_api'
+
+app = FastAPI(title='Rotate Captcha Api', openapi_url=f'{API_HEADERS}/openapi.json')
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*']
+)
+
+app.include_router(router)
+
+if __name__ == '__main__':
+    run(app, host='0.0.0.0', port=5555)
